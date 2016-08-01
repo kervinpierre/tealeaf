51f045fab2c17c28ea863217100a5c1dbbc5ecbe
phuong <phuonghqh@gmail.com>
2016-08-01 21:00:29 +0700

    Able to send & receive msg

--- src/main/java/com/github/phuonghuynh/jms/JmsConsumer.java
+++ src/main/java/com/github/phuonghuynh/jms/JmsConsumer.java
@@ -2,6 +2,7 @@ package com.github.phuonghuynh.jms;
 
 import com.github.phuonghuynh.model.Status;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
  * Created by phuonghqh on 7/26/16.
  */
 @Slf4j
+@Profile("JmsConsumer")
 @Component
 public class JmsConsumer {
 
